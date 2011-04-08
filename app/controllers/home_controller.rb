d91bc34aadb673e7596b8833314221160efee56d
Enrique Chavez <noone@tmeister.net>
2011-04-08 14:01:23 -0500

    Integracion del API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @some = ['1','2','3','4','5']
+    require 'instagram'
+    
+    Instagram.configure do |config|
+      config.client_id = "0447a7d130ae478580ab3a3f9cbd35cd"
+      config.client_secret = "45f96d2f48b641b496b92d9baf9adc94"
+    end
+    
+    @pictures = Instagram.media_popular
+
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,11 @@
 module HomeHelper
+
+  def strip_tags tags
+    ctags = ""
+    tags.each do |tag|
+      ctags << "#{tag} "
+    end
+    ctags
+  end
+
 end
